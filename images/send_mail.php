9057510a4f83eb044e81bcdfa88095e66c634c2e
markgee-ui <macgeengugi@gmail.com>
2025-09-26 09:37:15 +0300

    initial

---
+++ images/send_mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Your receiving email
+    $to = "milestonevisualsmedia@gmail.com"; 
+    $subject = "New Contact Form Submission from $name";
+
+    $body = "
+        <h2>New Message from Milestones Visuals Website</h2>
+        <p><strong>Name:</strong> $name</p>
+        <p><strong>Email:</strong> $email</p>
+        <p><strong>Message:</strong><br>$message</p>
+    ";
+
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<script>alert('Message sent successfully!'); window.location.href='index.html';</script>";
+    } else {
+        echo "<script>alert('Failed to send message. Try again later.'); window.location.href='index.html';</script>";
+    }
+}
+?>
